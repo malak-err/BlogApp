d77c08f4de6dadbafd3d4fc3076a660c8d748153
kawtarmouslim <kawtharmouslim33@gmail.com>
2025-04-23 18:00:32 +0100

    service get article

--- src/app/services/post-service.service.ts
+++ src/app/services/post-service.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from '../models/article';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostServiceService {
-
-  constructor() { }
+ private apiurl="http://localhost:3000";
+  constructor(private http:HttpClient) { }
+  getArticle():Observable<Article[]>{
+    return this.http.get<Article[]>(`${this.apiurl}${articles}`);
+  }
 }
