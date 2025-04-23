aa7f49da5c022cfbfec6da38d20c54d441ae728d
 <erraoudymalak1997@gmail.com>
2025-04-23 16:09:33 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PosteListComponent } from './poste-list/poste-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PosteListComponent,
+    PostDetailComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/poste-list/poste-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poste-list',
+  templateUrl: './poste-list.component.html',
+  styleUrls: ['./poste-list.component.css']
+})
+export class PosteListComponent {
+
+}
