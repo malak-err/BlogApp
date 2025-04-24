7475d6dbbfa2fd3d3e9bb006b46d8de3d728b81c
 <erraoudymalak1997@gmail.com>
2025-04-24 15:12:44 +0100

    toutes les fonctions crud

--- src/app/post-list/modifier/modifier.component.ts
+++ src/app/post-list/modifier/modifier.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Article } from 'src/app/models/article';
+import { PostServiceService } from 'src/app/services/post-service.service';
 @Component({
   selector: 'app-modifier',
   templateUrl: './modifier.component.html',
   styleUrls: ['./modifier.component.css']
 })
-export class ModifierComponent {
+export class ModifierComponent implements OnInit {
+
+   article!:Article;
+   id!:number;
+  constructor(private postService: PostServiceService,
+      private route: ActivatedRoute,private router: Router,){
+  
+    }
+  ngOnInit(): void{
+    this.update();
+  }
+ update(){
+  this.postService.updateArticle(this.id ,this.article).subscribe(response=>{
+    this.router.navigate(['/post-list/list/clist'])
+  })}
 
 }
+
